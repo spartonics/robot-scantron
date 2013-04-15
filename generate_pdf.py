a259ac8ffd3a75dd00ad288c345e7bf37bba5a87
Philipp Schrader <philipp.schrader@gmail.com>
2013-04-14 17:45:47 -0700

    Experimenting with multiple boxes in a row.

--- generate_pdf.py
+++ generate_pdf.py
@@ -7,27 +7,42 @@ from reportlab.pdfgen import canvas
 def ty(y):
     return 11*inch - y
 
-def drawBoolean(c, x, y, label="UNKNOWN"):
+def drawBox(c, x, y, label='', length=0.4*inch):
+    offset = (length - fontSize) / 2
+    c.drawCentredString(x + length / 2, ty(y), label)
+    c.rect(x, ty(y + offset), length, length, stroke=1, fill=0)
+
+def drawBoolean(c, x, y, label="UNKNOWN", spacing=0.4):
     x = x * inch
     y = y * inch
 
     yes_x = x + 2*inch
     yes_y = y
 
-    no_x = x + 2.2*inch
+    no_x = x + (2.0 + spacing) * inch
     no_y = y
 
-    box_length = 0.2*inch
+    c.drawString(x, ty(y), label)
+
+    drawBox(c, yes_x, yes_y, label='Y')
+    drawBox(c, no_x, no_y, label='N')
+
+def drawInteger(c, x, y, label="UNKNOWN", spacing=0.4):
+    x = x * inch
+    y = y * inch
 
     c.drawString(x, ty(y), label)
-    c.drawString(yes_x, ty(yes_y), 'Y')
-    c.drawString(no_x, ty(no_y), 'N')
 
-    c.rect(yes_x, ty(yes_y), box_length, box_length, stroke=1, fill=0)
+    for i in range(10):
+        drawBox(c, x + (2 + i * spacing) * inch, y, label='%d' % i)
+
+fontSize = 0.15 * inch
 
 canvas = canvas.Canvas('form.pdf', pagesize=letter)
+canvas.setFontSize(fontSize)
 
-drawBoolean(canvas, 2, 2, 'Broke Down')
-drawBoolean(canvas, 2, 2+0.2, 'Disqualified')
+drawBoolean(canvas, 1, 2, 'Broke Down')
+drawBoolean(canvas, 1, 2+0.2, 'Disqualified')
+drawInteger(canvas, 1, 2+0.4, 'Pyramid')
 
 canvas.save()
