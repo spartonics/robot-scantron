94a8a7e91083fbc594f38305c214f5d10df68877
Philipp Schrader <philipp.schrader@gmail.com>
2013-04-14 18:17:15 -0700

    The scantron fields are now data-driven.

--- generate_pdf.py
+++ generate_pdf.py
@@ -5,31 +5,33 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
 class Scantron:
-    def __init__(self, filename):
+    def __init__(self, filename, spacing=0.2):
         self._fontSize = 0.15*inch
 
         self._canvas = canvas.Canvas(filename, pagesize=letter)
         self._canvas.setFontSize(self._fontSize)
 
+        self._y = 1 * inch
+        self._spacing = spacing * inch
 
-    def _y(self, y):
+    def _to_y(self, y):
         return 11*inch - y
 
 
     def drawBox(self, x, y, label='', length=0.4*inch):
         offset = (length - self._fontSize) / 2
-        self._canvas.drawCentredString(x + length / 2, self._y(y), label)
-        self._canvas.rect(x, self._y(y + offset), length, length, stroke=1, 
+        self._canvas.drawCentredString(x + length / 2, self._to_y(y), label)
+        self._canvas.rect(x, self._to_y(y + offset), length, length, stroke=1, 
                 fill=0)
 
 
     def drawText(self, x, y, text=''):
-        self._canvas.drawString(x, self._y(y), text)
+        self._canvas.drawString(x, self._to_y(self._y), text)
 
 
-    def drawBoolean(self, x, y, label="UNKNOWN", spacing=0.4):
+    def drawBoolean(self, x, label="UNKNOWN", spacing=0.4):
         x = x * inch
-        y = y * inch
+        y = self._y
 
         yes_x = x + 2*inch
         yes_y = y
@@ -41,23 +43,58 @@ class Scantron:
         self.drawBox(yes_x, yes_y, label='Y')
         self.drawBox(no_x, no_y, label='N')
 
+        self._y += self._spacing
 
-    def drawInteger(self, x, y, label="UNKNOWN", spacing=0.4):
+
+    def drawInteger(self, x, label="UNKNOWN", spacing=0.4):
         x = x * inch
-        y = y * inch
+        y = self._y
 
         self.drawText(x, y, label)
 
         for i in range(10):
             self.drawBox(x + (2 + i * spacing) * inch, y, label='%d' % i)
 
+        self._y += self._spacing
+
+
+    def drawQR(self, x, y, data):
+        pass
+
 
     def save(self):
         self._canvas.save()
 
 
-scantron = Scantron('form.pdf')
-scantron.drawBoolean(1, 2, 'Broke Down')
-scantron.drawBoolean(1, 2+0.2, 'Disqualified')
-scantron.drawInteger(1, 2+0.4, 'Pyramid')
+class Field:
+    def __init__(self, name, label, fieldType):
+        self.name = name
+        self.label = label
+        self.fieldType = fieldType
+
+data = [
+        Field('auton_high', 'Auton High', int),
+        Field('auton_mid', 'Auton Mid', int),
+        Field('auton_low', 'Auton Low', int),
+        Field('high', 'High', int),
+        Field('mid', 'Mid', int),
+        Field('low', 'Low', int),
+        Field('pyramid', 'Pyramid', int),
+        Field('missed', 'Missed', int),
+        Field('fouls', 'Fouls', int),
+        Field('tech_fouls', 'Tech fouls', int),
+        Field('defense', 'Defense', bool),
+        Field('pickup', 'Pickup', bool),
+        Field('noshow', 'Noshow', bool),
+        Field('brokedown', 'Brokedown', bool),
+        Field('dq', 'DQ', bool),
+]
+
+
+scantron = Scantron('form.pdf', spacing=0.5)
+for field in data:
+    if field.fieldType == int:
+        scantron.drawInteger(1, field.label)
+    elif field.fieldType == bool:
+        scantron.drawBoolean(1, field.label)
 scantron.save()
