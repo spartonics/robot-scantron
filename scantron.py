29daedadc38c4536e784cb24a7155c46d0b736ce
Philipp Schrader <philipp.schrader@gmail.com>
2013-05-04 10:29:14 -0700

    A QR code with match and position is now in TR corner.

--- scantron.py
+++ scantron.py
@@ -3,6 +3,8 @@
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from qrcode import *
+
 
 class Scantron:
     def __init__(self, filename, spacing=0.3*inch):
@@ -31,12 +33,18 @@ class Scantron:
             self._canvas.drawCentredString(x + size / 2, self._to_y(y), label)
 
         y += offset
-        self._canvas.rect(x, self._to_y(y), size, size, stroke=1, 
-                fill=int(filled))
+        if filled:
+            self._canvas.rect(x, self._to_y(y), size, size, stroke=0, fill=1)
+        else:
+            self._canvas.rect(x, self._to_y(y), size, size, stroke=1, fill=0)
 
 
-    def draw_text(self, x, y, text=''):
+    def draw_text(self, x, y, text='', size=0):
+        if size != 0:
+            self._canvas.setFontSize(size)
+
         self._canvas.drawString(x, self._to_y(y), text)
+        self._canvas.setFontSize(self._fontSize)
 
 
     def draw_boolean(self, label="UNKNOWN", x=1*inch):
@@ -63,8 +71,22 @@ class Scantron:
         self._y += self._spacing
 
 
-    def draw_qr(self, x, y, data):
-        pass
+    def draw_qr(self, x, y, data, unit_size=0.03*inch):
+        qr = QRCode(version=2, error_correction=ERROR_CORRECT_L)
+        qr.add_data(data)
+        qr.make()
+
+        qr_x = x
+        qr_y = y
+
+        for row in qr.modules:
+            for col in row:
+                if col:
+                    self.draw_box(qr_x, qr_y, size=unit_size, filled=True)
+                qr_x += unit_size
+
+            qr_x = x
+            qr_y += unit_size
 
 
     def adjust_y(self, delta_y):
@@ -78,12 +100,16 @@ class Scantron:
 
     def populate(self, data):
         # Draw boxes for determining boundaries
-        self.draw_box(1*inch, 1*inch, size=0.4*inch, filled=True)
-        self.draw_box((7-0.4)*inch, (10-0.4)*inch, size=0.4*inch, filled=True)
+        self.draw_box(1*inch, 1.2*inch, size=0.4*inch, filled=True)
+        self.draw_box((7.5-0.4)*inch, (10.2-0.4)*inch, size=0.4*inch, 
+                filled=True)
+
+        # Draw the QR code containing match and position
+        self.draw_qr(6.5*inch, 1*inch, '1:1')
 
         # Draw a sheet title
-        self.draw_text(2*inch, 1*inch, 'Spartonics 1503')
-        self.draw_text(2*inch, 1.4*inch, 'Scouting Sheet')
+        self.draw_text(2*inch, 1.2*inch, 'Spartonics 1503', size=0.3*inch)
+        self.draw_text(2*inch, 1.6*inch, 'Scouting Sheet', size=0.3*inch)
         self.adjust_y(1.0*inch)
 
         # Create the bubbles for the team number
